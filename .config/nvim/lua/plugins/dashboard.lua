1939be3aa92ff4a210e8a075684cdcdc705fb1a2
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-04-01 13:25:54 +0200

    vim: change dashboard

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,20 +1,7 @@
 return {
-	"nvimdev/dashboard-nvim",
-	event = "VimEnter",
-	config = function()
-
-		require("dashboard").setup({
-			theme = "hyper",
-			config = {
-				week_header = {
-					enable = true,
-				},
-				shortcut = {
-					{ desc = "󰚰 Update", group = "@property", action = "Lazy update", key = "u" },
-					{ desc = " dotfiles", group = "@property", action = "e ~/dotfiles", key = "d" },
-				},
-			},
-		})
-	end,
-	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+  "goolord/alpha-nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("alpha").setup(require("alpha.themes.startify").config)
+  end,
 }
