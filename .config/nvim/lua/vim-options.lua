a6e6360a03ed6e3f142688bbf36b522d2db57638
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-06-20 20:25:26 +0200

    vim: change remaps

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -1,10 +1,10 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
 vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.mouse = "a"
+-- vim.opt.clipboard = 'unnamedplus'
 vim.opt.undofile = true
 
 vim.opt.ignorecase = true
@@ -16,27 +16,27 @@ vim.opt.splitbelow = true
 
 vim.opt.scrolloff = 8
 
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<A-Space>', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "<A-Space>", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
   callback = function()
     vim.highlight.on_yank()
   end,
@@ -46,10 +46,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- vim.cmd("nmap <Down>  <Nop>")
 -- vim.cmd("nmap <Left>  <Nop>")
 -- vim.cmd("nmap <Right> <Nop>")
-vim.cmd("map $ <Nop>")
-vim.cmd("map ^ <Nop>")
-vim.cmd("map { <Nop>")
-vim.cmd("map } <Nop>")
+-- vim.cmd("map $ <Nop>")
+-- vim.cmd("map ^ <Nop>")
+-- vim.cmd("map { <Nop>")
+-- vim.cmd("map } <Nop>")
 vim.cmd("noremap K     {")
 vim.cmd("noremap J     }")
 vim.cmd("noremap H     ^")
@@ -70,7 +70,37 @@ vim.cmd("nnoremap <S-Tab> <<")
 vim.cmd("vnoremap <Tab>   >><Esc>gv")
 vim.cmd("vnoremap <S-Tab> <<<Esc>gv")
 
-vim.cmd("nnoremap <A-j> :m .+1<CR>==")
-vim.cmd("nnoremap <A-k> :m .-2<CR>==")
-vim.cmd("vnoremap <A-j> :m '>+1<CR>gv=gv")
-vim.cmd("vnoremap <A-k> :m '<-2<CR>gv=gv")
+-- vim.cmd("nnoremap <A-j> :m .+1<CR>==")
+-- vim.cmd("nnoremap <A-k> :m .-2<CR>==")
+-- vim.cmd("vnoremap <A-j> :m '>+1<CR>gv=gv")
+-- vim.cmd("vnoremap <A-k> :m '<-2<CR>gv=gv")
+
+vim.cmd("command! W w")
+vim.cmd("command! Wa wa")
+vim.cmd("command! Wqa wqa")
+
+vim.keymap.set({ "n", "v" }, "<A-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set({ "n", "v" }, "<A-k>", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+
+vim.keymap.set("n", "<A-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set(
+    "n",
+    "<leader>ee",
+    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+)
+
