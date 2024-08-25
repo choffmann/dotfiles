e874128ebf5271d1d341aa101bf516ea06080852
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-08-25 20:35:32 +0200

    vim: change harpoon and telescope config

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -7,7 +7,7 @@ return {
     harpoon:setup()
     vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Add Harpoon"})
     vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-    vim.keymap.set("n", "<C-f>", function() harpoon:list():prev() end)
-    vim.keymap.set("n", "<C-b>", function() harpoon:list():next() end)
+    vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+    vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
   end
 }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -47,14 +47,12 @@ return {
       vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
       vim.keymap.set("n", "<leader>st", builtin.colorscheme, { desc = "[S]earch [T]hemes" })
+      vim.keymap.set("n", "<leader>scc", builtin.commands, { desc = "[S]earch [C]ommands" })
+      vim.keymap.set("n", "<leader>sch", builtin.command_history, { desc = "[S]earch [C]ommand [H]istory" })
 
       vim.keymap.set("n", "<leader>sn", function()
         builtin.find_files({ cwd = vim.fn.stdpath("config") })
       end, { desc = "[S]earch [N]eovim files" })
-
-      vim.keymap.set("n", "<leader>so", function()
-        builtin.find_files({ cwd = vim.fn.expand("~/obsidian") })
-      end, { desc = "[S]earch [O]bsidian files" })
     end,
   },
   {
